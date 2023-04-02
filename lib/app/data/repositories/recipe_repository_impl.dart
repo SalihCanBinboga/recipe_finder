42c55b69631131d84caff4cb5431d3277c59c09f
Salih Can <salihcn.bnb@gmail.com>
2023-04-02 13:31:52 +0100

    feat: create recipe repository implementation

---
+++ lib/app/data/repositories/recipe_repository_impl.dart
@@ -0,0 +1,53 @@
+import '../../domain/models/recipe_entity/recipe_entity.dart';
+import '../../domain/repositories/recipe_repository.dart';
+import '../data_sources/local/local_recipe_favorite_data_source.dart';
+import '../data_sources/models/recipe_response.dart';
+import '../data_sources/remote/remote_recipe_data_source.dart';
+
+class RecipeRepositoryImpl implements RecipeRepository {
+  final RemoteRecipeDataSource _remoteDataSource;
+  final LocalRecipeFavoriteDataSource _localFavoriteDataSource;
+
+  RecipeRepositoryImpl(this._remoteDataSource, this._localFavoriteDataSource);
+
+  @override
+  Future<void> addFavoriteRecipe(RecipeEntity recipe) async {
+    await _localFavoriteDataSource.saveRecipe(recipe.toJson());
+  }
+
+  @override
+  Future<Iterable<RecipeEntity>> getFavoriteRecipes() async {
+    final result = await _localFavoriteDataSource.getFavoriteRecipes();
+
+    final recipes = result.map((recipe) => RecipeResponse.fromJson(recipe));
+
+    return recipes.map(
+      (recipe) => RecipeEntity.fromRecipeResponse(recipe),
+    );
+  }
+
+  @override
+  Future<RecipeEntity> getRecipeDetails(String recipeId) async {
+    final result = await _remoteDataSource.getRecipe(recipeId: recipeId);
+    final recipe = RecipeResponse.fromJson(result);
+    return RecipeEntity.fromRecipeResponse(recipe);
+  }
+
+  @override
+  Future<void> removeFavoriteRecipe(RecipeEntity recipe) {
+    return _localFavoriteDataSource.removeRecipe(recipe.id);
+  }
+
+  @override
+  Future<Iterable<RecipeEntity>> getRecipes() async {
+    // TODO: 1.04.2023 21:56 May create a cache for this
+
+    final result = await _remoteDataSource.getRecipes();
+
+    final recipes = result.map((recipe) => RecipeResponse.fromJson(recipe));
+
+    return recipes.map(
+      (recipe) => RecipeEntity.fromRecipeResponse(recipe),
+    );
+  }
+}
