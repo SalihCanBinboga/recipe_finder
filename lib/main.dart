0d767922099e798948549a2b87609a5429b84c73
Salih Can <salihcn.bnb@gmail.com>
2023-04-13 14:20:35 +0300

    feat: setup dependency injection package

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'app/init/dependency_injection/dependency_injection.dart';
 import 'app/init/recipe_finder_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
+  configureDependencies();
 
   runApp(const RecipeFinderApp());
 }
