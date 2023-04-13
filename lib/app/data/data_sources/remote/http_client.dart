f3b487cba4ceabf0c4aa636a1cb91e41188d0f1d
Salih Can <salihcn.bnb@gmail.com>
2023-04-13 14:22:45 +0300

    feat: create http client module to create http client instance

--- lib/app/data/data_sources/remote/http_client.dart
+++ lib/app/data/data_sources/remote/http_client.dart
@@ -3,51 +3,9 @@ import 'package:injectable/injectable.dart';
 
 @Singleton()
 class HttpClient {
-  static final HttpClient _singleton = HttpClient._internal();
+  final Dio _client;
 
-  factory HttpClient({required String baseUrl}) {
-    final dio = _singleton._client;
-    dio.options.baseUrl = baseUrl;
-
-    for (final apiKey in ApiKeys.values) {
-      dio.options.headers.addAll(
-        {
-          apiKey.name: apiKey.value,
-        },
-      );
-    }
-
-    dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) {
-          if (kDebugMode) {
-            print('REQUEST[${options.method}] => PATH: ${options.path}');
-          }
-          return handler.next(options);
-        },
-        onResponse: (response, handler) {
-          if (kDebugMode) {
-            print('RESPONSE[${response.statusCode}] => '
-                'PATH: ${response.requestOptions.path}');
-          }
-          return handler.next(response);
-        },
-        onError: (DioError e, handler) {
-          if (kDebugMode) {
-            print('ERROR[${e.response?.statusCode}] => '
-                'PATH: ${e.requestOptions.path}');
-          }
-          return handler.next(e);
-        },
-      ),
-    );
-
-    return _singleton;
-  }
-
-  final Dio _client = Dio();
-
-  HttpClient._internal();
+  HttpClient(this._client);
 
   Future<Response> get({
     String path = '',
