96fb067679c6b2c9a132ac5ce23f2070fa345bdf
Salih Can <salihcn.bnb@gmail.com>
2023-04-13 14:17:50 +0300

    feat: create debounce manager

--- lib/app/data/data_sources/constants/api_keys.dart
+++ lib/app/data/data_sources/constants/api_keys.dart
@@ -1,6 +1,7 @@
 enum ApiKeys {
   appId,
-  appKey;
+  appKey,
+  recipesBaseUrl;
 
   String get value {
     switch (this) {
@@ -8,6 +9,8 @@ enum ApiKeys {
         return const String.fromEnvironment('app_id', defaultValue: '');
       case ApiKeys.appKey:
         return const String.fromEnvironment('app_key', defaultValue: '');
+      case ApiKeys.recipesBaseUrl:
+        return 'https://api.edamam.com/api/recipes/v2';
     }
   }
 
@@ -17,6 +20,19 @@ enum ApiKeys {
         return 'app_id';
       case ApiKeys.appKey:
         return 'app_key';
+      case ApiKeys.recipesBaseUrl:
+        throw Exception('baseUrl is not an apiKey');
+    }
+  }
+
+  bool get isForHttpHeaders {
+    switch (this) {
+      case ApiKeys.appId:
+        return true;
+      case ApiKeys.appKey:
+        return true;
+      case ApiKeys.recipesBaseUrl:
+        return false;
     }
   }
 }
