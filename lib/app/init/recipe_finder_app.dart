6407df8cb284253206cd68d99c18ddff82336b24
Salih Can <salihcn.bnb@gmail.com>
2023-04-14 14:16:59 +0300

    feat: integrate route manager class to material app

--- lib/app/init/recipe_finder_app.dart
+++ lib/app/init/recipe_finder_app.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_finder/app/presentation/screens/home/home_view.dart';
+
+import '../presentation/router/route_manager.dart';
 
 class RecipeFinderApp extends StatelessWidget {
   const RecipeFinderApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: 13.04.2023 15:05 Create named routes structure.
     return MaterialApp(
-      home: HomeView(),
+      initialRoute: RouteManager.initialRoute,
+      onGenerateRoute: RouteManager.generateRoute,
     );
   }
 }
